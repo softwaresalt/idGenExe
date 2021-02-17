bf6032241048c8a72f3150a5e206219324d148c7
williamsderek <42183845+williamsderek@users.noreply.github.com>
2021-02-16 22:36:54 -0800

    Add project files.

---
+++ IdGen/IIdGenerator.cs
@@ -0,0 +1,21 @@
+﻿//Source: https://github.com/RobThree/IdGen
+using System.Collections.Generic;
+
+namespace IdGen
+{
+	/// <summary>
+	/// Provides the interface for Id-generators.
+	/// </summary>
+	/// <typeparam name="T">The type for the generated ID's.</typeparam>
+#pragma warning disable CA1710 // Identifiers should have correct suffix
+
+	public interface IIdGenerator<T> : IEnumerable<T>
+#pragma warning restore CA1710 // Identifiers should have correct suffix
+	{
+		/// <summary>
+		/// Creates a new Id.
+		/// </summary>
+		/// <returns>Returns an Id.</returns>
+		T CreateId();
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.Arguments;
+using System.IO;
+using System.Linq;
+
+namespace IdGen
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			ParserOptions options = new ParserOptions(true, "--", "-", "=");
+			Parser parser = new Parser(options);
+			NamedArgument<int> count = new NamedArgument<int>("gen-count", "gc", "Gencount", "The count of IDs to generate; default=100.", 100, null);
+			NamedArgument<string> o = new NamedArgument<string>("output", "o", "Ouput", "The file location to output the results of the ID generator.", default, null);
+			FlagArgument<bool> help = new FlagArgument<bool>("help", "h", "Help", "Output parameter options to app.");
+			(parser.NamedArguments as List<Argument>).Add(count);
+			(parser.NamedArguments as List<Argument>).Add(o);
+			(parser.FlagArguments as List<Argument>).Add(help);
+			List<string> parms = new List<string>();
+			string exe = Environment.GetCommandLineArgs()[0];
+			FileInfo info = new FileInfo(exe);
+			parms.Add(info.Name.Replace(info.Extension, ".exe")); //Parser expects first argument to be the name of the executable.
+			if (parms.Count == 0 || ((!args.Contains("-gc") || !args.Contains("--gen-count")) && !(args.Contains("-h") || args.Contains("--help"))))
+			{
+				parms.Add($"-{count.Alias}");
+			}
+			parms.AddRange(args.ToList());
+			ParsingResults parsingResults = parser.Parse(parms.ToArray());
+			if (parsingResults.ParsedValues.ContainsKey(help.Destination))
+			{
+				writeHelpInfo(parser);
+				return;
+			}
+
+			int gc = parsingResults.GetParsedValue<int>(count.Destination);
+
+			//IdGenerator initializer can be improved to include custom IdGeneratorOptions.
+			IdGenerator idGen = new IdGenerator(0);
+			IEnumerable<long> ids = idGen.Take(gc);
+
+			if (parsingResults.ParsedValues.ContainsKey(o.Destination))
+			{
+				writeToFile(ids, parsingResults.GetParsedValue<string>(o.Destination));
+			}
+			else
+			{
+				foreach (long id in ids) { Console.WriteLine(id); }
+			}
+		}
+
+		private static void writeHelpInfo(Parser parser)
+		{
+			if (parser.NamedArguments.Any()) { Console.WriteLine("Arguments"); }
+			foreach (Argument arg in parser.NamedArguments)
+			{
+				Console.WriteLine($"\t--{arg.Name} -{arg.Alias}\t\t: {arg.Help}");
+			}
+			if (parser.NamedArguments.Any())
+			{
+				Console.WriteLine();
+				Console.WriteLine(@"Example: idGenExe.exe -gc=100 -o=C:\temp\ids.txt");
+				Console.WriteLine();
+			}
+
+			if (parser.PositionalArguments.Any()) { Console.WriteLine("PositionalArguments"); }
+			foreach (Argument arg in parser.PositionalArguments)
+			{
+				Console.WriteLine($"\t--{arg.Name} -{arg.Alias}\t\t: {arg.Help}");
+			}
+
+			if (parser.FlagArguments.Any()) { Console.WriteLine("Flags"); }
+			foreach (Argument arg in parser.FlagArguments)
+			{
+				Console.WriteLine($"\t--{arg.Name} -{arg.Alias}\t\t: {arg.Help}");
+			}
+
+			if (parser.Commands.Any()) { Console.WriteLine("Commands"); }
+			foreach (Command cmd in parser.Commands)
+			{
+				Console.WriteLine($"\t--{cmd.Name} -{cmd.Alias}");
+			}
+		}
+
+		private static void writeToFile(IEnumerable<long> ids, string path)
+		{
+			FileInfo info = new FileInfo(path);
+			if (info.Directory.Exists)
+			{
+				if (!info.Exists) { File.AppendAllText(path, String.Empty); }
+				using (StreamWriter writer = info.AppendText())
+				{
+					foreach (long id in ids)
+					{
+						writer.WriteLine(id);
+					}
+				}
+			}
+			else
+			{
+				throw new InvalidOperationException($"The file and path ({path}) do not exist.");
+			}
+		}
+	}
+}
